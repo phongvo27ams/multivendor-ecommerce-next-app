f4bb45c2399f23ded1fcb737aaf01aab3a7d3d4d
Phong Vo <phongvo27ams@gmail.com>
2025-12-05 11:59:09 +0700

    feat: implement coupon management with CRUD operations and admin authentication

---
+++ app/api/admin/coupon/route.js
@@ -0,0 +1,86 @@
+import { getAuth } from "@clerk/nextjs/server";
+import authAdmin from "../../../../middlewares/authAdmin";
+import prisma from "../../../../lib/prisma";
+import { NextResponse } from "next/server";
+
+// Create a new coupon
+export async function POST(request) {
+  try {
+    const { userId } = getAuth(request);
+    const isAdmin = await authAdmin(userId);
+
+    if (!isAdmin) {
+      return NextResponse.json(
+        { message: "Unauthorized access" },
+        { status: 403 }
+      );
+    }
+
+    const { coupon } = await request.json();
+    coupon.code = coupon.code.toUpperCase();
+
+    await prisma.coupon.create({
+      data: coupon,
+    });
+
+    return NextResponse.json({ message: "Coupon created successfully" }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating coupon:", error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
+
+// Delete a coupon: DELETE /api/coupon?id=couponId
+export async function DELETE(request) {
+  try {
+    const { userId } = getAuth(request);
+    const isAdmin = await authAdmin(userId);
+
+    if (!isAdmin) {
+      return NextResponse.json(
+        { message: "Unauthorized access" },
+        { status: 403 }
+      );
+    }
+
+    const { searchParams } = request.nextUrl;
+    const code = searchParams.get("code");
+
+    await prisma.coupon.delete({
+      where: { code },
+    });
+    return NextResponse.json({ message: "Coupon deleted successfully" }, { status: 200 });
+  } catch (error) {
+    console.error("Error deleting coupon:", error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
+
+export async function GET(request) {
+  try {
+    const { userId } = getAuth(request);
+    const isAdmin = await authAdmin(userId);
+
+    if (!isAdmin) {
+      return NextResponse.json(
+        { message: "Unauthorized access" },
+        { status: 403 }
+      );
+    }
+
+    const coupons = await prisma.coupon.findMany();
+    return NextResponse.json({ coupons }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching coupons:", error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
